df25ddd8e26425430f39e4d5c430c6a2b57efe78
VulpineReynard <milare8@gmail.com>
2019-08-14 16:18:50 -0400

    setup initial state with redux and subbed in props to reducer's default return

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import { mainReducer } from './Reducers/mainReducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+export const store = createStore(mainReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  rootElement
+);
