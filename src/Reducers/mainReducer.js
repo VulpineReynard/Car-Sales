6a11291a4268cd7813034766d5d93ea4bde5e0e9
VulpineReynard <milare8@gmail.com>
2019-08-14 17:19:18 -0400

    MVP Complete

--- src/Reducers/mainReducer.js
+++ src/Reducers/mainReducer.js
@@ -15,8 +15,43 @@ const initialState = {
   ]
 };
 
+const alreadyContainsFeature = (currentFeatures, feature) => {
+  currentFeatures.forEach(currentFeature => {
+    if (currentFeature.id === feature.id) return true;
+  });
+}
+
 export const mainReducer = (state = initialState, action) => {
   switch(action.type) {
+    case "ADD_FEATURE":
+      if (alreadyContainsFeature(state.car.features, action.payload)) {
+        alert('Feature already added!');
+        return { ...state };
+      }
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice + action.payload.price,
+        car: {
+          ...state.car,
+          features: [
+          ...state.car.features, 
+          action.payload
+        ]
+      },
+    };
+
+    case "REMOVE_FEATURE":
+      console.log(action.payload)
+      console.log(state.car.features)
+      return {
+        ...state,
+        additionalPrice: state.additionalPrice - action.payload.price,
+        car: {
+          ...state.car,
+          features: state.car.features.filter(feature => feature.id !== action.payload.id)
+        },
+      };
+
     default:
       return state;
   }
--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { removeFeature } from '../Actions/mainActions';
 import AddedFeature from './AddedFeature';
 
 const AddedFeatures = props => {
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.features.length ? (
+      {props.features.length >= 1 ? (
         <ol type="1">
-          {props.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+          {props.features.map((item, index) => (
+            <AddedFeature removeFeature={props.removeFeature} key={index} feature={item} />
           ))}
         </ol>
       ) : (
@@ -28,5 +28,5 @@ const mapStateToProps = state => {
 
 export default connect(
   mapStateToProps,
-  {  }
+  { removeFeature }
 )(AddedFeatures);
